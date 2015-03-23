aa8ded44bbf6d250a3618b08f4e7128c2790e4ad
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-03-23 18:31:43 +0600

    Sh

--- modules/_May/may_pages/js/may_sh.js
+++ modules/_May/may_pages/js/may_sh.js
@@ -12,54 +12,10 @@
          //console.log('href = ' + $(this).attr('href'));
          //console.log('title = ' + $(this).attr('title'));
 
-         (jQuery).ajax({
-            
-                url: 'http://smart1977.ddns.net', 
-                data: {
-                        access: 'xxx',
-                        op: 4,
-                        lamp: 0
-                        //id: $(this).attr('id'),
-                        //class: $(this).attr('class'),
-                        //click_page: window.location.href,
-                        //url: $(this).attr('href'),
-                        //title: $(this).attr('title')
-                        //,referer: document.referrer
-                       
-                      }, 
-                    type: 'GET', 
-                    //dataType: 'json'
-                    dataType: 'jsonp'
-                    
-                    , 
-                    jsonp: false,
-                    jsonpCallback: "myJsonMethod",
-                    success: function(data) 
-                            { 
-                                console.dir(data);
-                                //data = JSON.parse(data);
-                                console.log(data);
-                                console.log(data.status);
-                                console.log(data['params']);
-                                
-                                for (var key in data) {
-                                  if (data.hasOwnProperty(key)) {
-                                    console.log(key + ': ' + data[key]);
-                                    //alert(data[key].msg);
-                                  }
-                                }
-                                
-                                if(!data.error) {
-                                    console.log('NEW The data is arrived!');
-                                }
-                                return false;
-                            }
-                     ,error: function(e) { console.log(e.message); }
-                     
-            }); // end of (jQuery).ajax
+         
             
             
-          /*
+          
          (jQuery).ajax({
             
                 url: '/sh-get', 
@@ -67,6 +23,7 @@
                         type: 'button_click',
                         id: $(this).attr('id'),
                         class: $(this).attr('class'),
+                        access: 'xxx'
                         //click_page: window.location.href,
                         //url: $(this).attr('href'),
                         //title: $(this).attr('title')
@@ -80,14 +37,72 @@
                     success: function(data) 
                             { 
                                 console.log(data);
-                                if(!data.error) {
-                                    console.log('The data is arrived!');
+                                console.dir(data);
+                                
+                                if(!data.error) { 
+                                                  
+                                                        console.log('The data is arrived from the middle Server!');
+                                  
+                                                        return;
+                                                        
+                                                        
+                                    
+                                                        (jQuery).ajax({
+
+                                                                url: 'http://smart1977.ddns.net', 
+                                                                data: {
+                                                                        access: 'xxx',
+                                                                        op: 4,
+                                                                        lamp: 0
+                                                                        //id: $(this).attr('id'),
+                                                                        //class: $(this).attr('class'),
+                                                                        //click_page: window.location.href,
+                                                                        //url: $(this).attr('href'),
+                                                                        //title: $(this).attr('title')
+                                                                        //,referer: document.referrer
+
+                                                                      }, 
+                                                                    type: 'GET', 
+                                                                    //dataType: 'json'
+                                                                    dataType: 'jsonp'
+
+                                                                    , 
+                                                                    jsonp: false,
+                                                                    jsonpCallback: "myJsonMethod",
+                                                                    success: function(data) 
+                                                                            { 
+                                                                                console.dir(data);
+                                                                                //data = JSON.parse(data);
+                                                                                console.log(data);
+                                                                                console.log(data.status);
+                                                                                
+                                                                                for (var key in data) {
+                                                                                  if (data.hasOwnProperty(key)) {
+                                                                                    console.log(key + ': ' + data[key]);
+                                                                                    //alert(data[key].msg);
+                                                                                  }
+                                                                                }
+
+                                                                                if(!data.error) {
+                                                                                    console.log('NEW The data is arrived from Arduino!');
+                                                                                }
+                                                                                return false;
+                                                                            }
+                                                                     ,error: function(e) { console.log(e.message); }
+
+                                                            }); // end of (jQuery).ajax
+                                    
                                 }
                                 return false;
                             } 
                      
             }); // end of (jQuery).ajax
-        */
+        
+        
+        
+        
+        
+        
             return false;
 
        }); // End of $(".light").click(function(){
