4cf34ba5febe20acd49ba07a6c6223c525bb8779
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-21 23:08:23 +0600

    ckeditor

--- modules/_Editor/ckeditor/ckeditor/config.js
+++ modules/_Editor/ckeditor/ckeditor/config.js
@@ -8,6 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
   
-  config.allowedContent = 'a[*]{*}(*)';
-  config.extraAllowedContent = 'a[*]{*}(*)';
+  //config.allowedContent = 'a[*]{*}(*)';
+  //config.extraAllowedContent = 'a[*]{*}(*)';
+  config.extraAllowedContent = 'a[*]';
 };
