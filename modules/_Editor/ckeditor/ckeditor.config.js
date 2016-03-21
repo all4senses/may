799d9eb1a90296776104be0fab316d152c0294cf
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-21 23:20:36 +0600

    ckeditor

--- modules/_Editor/ckeditor/ckeditor.config.js
+++ modules/_Editor/ckeditor/ckeditor.config.js
@@ -31,7 +31,9 @@ CKEDITOR.editorConfig = function(config) {
   
   //config.extraAllowedContent = 'a[*]{*}(*)';
   //config.extraAllowedContent = 'a[*]';
-  config.allowedContent = 'a[*]{*}(*)';
+  
+  
+  config.allowedContent = 'a[*]{*}(*), img[*]{*}(*)';
   
   config.codemirror = {
 
