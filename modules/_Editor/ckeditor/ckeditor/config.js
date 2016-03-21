ace22eaf050c3a087a88f2b00b4b60224397d6c9
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-21 23:10:28 +0600

    ckeditor

--- modules/_Editor/ckeditor/ckeditor/config.js
+++ modules/_Editor/ckeditor/ckeditor/config.js
@@ -9,6 +9,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
   
   //config.allowedContent = 'a[*]{*}(*)';
+  config.allowedContent = 'a[*]';
   //config.extraAllowedContent = 'a[*]{*}(*)';
-  config.extraAllowedContent = 'a[*]';
+  //config.extraAllowedContent = 'a[*]';
 };
