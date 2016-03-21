543ecb9fe0903642a43370a7a910a3bb3c173e27
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-21 06:23:33 +0600

    Add/update modules

--- modules/_Editor/ckeditor/ckeditor.config.js
+++ modules/_Editor/ckeditor/ckeditor.config.js
@@ -26,7 +26,8 @@ CKEDITOR.editorConfig = function(config) {
 
   // [#1762328] Uncomment the line below to protect <code> tags in CKEditor (hide them in wysiwyg mode).
   // config.protectedSource.push(/<code>[\s\S]*?<\/code>/gi);
-  config.extraPlugins = '';
+//  config.extraPlugins = '';
+  config.extraPlugins = 'widget';
 
   /*
     * Append here extra CSS rules that should be applied into the editing area.
