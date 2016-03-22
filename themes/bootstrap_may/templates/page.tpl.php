e86d9874c2e933220cf083cbfcd59bb2c0bc26d2
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-22 23:19:40 +0600

    Theming

--- themes/bootstrap_may/templates/page.tpl.php
+++ themes/bootstrap_may/templates/page.tpl.php
@@ -73,7 +73,7 @@
  * @ingroup templates
  */
 ?>
-xxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="<?php print $container_class; ?>">
     <div class="navbar-header">
