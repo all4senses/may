c11ca15f8b13b0d26eed08e77ae43591e957ec33
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-23 14:53:27 +0600

    Scrolly testing

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -9,5 +9,11 @@
  * Implements hook_preprocess_page().
  */
 function bootstrap_may_preprocess_page(&$variables) {
-  dpm($variables);
+  //dpm($variables);
+  
+  if ($variables['is_front']) {
+    //drupal_add_js(libraries_get_path('select2').'/select2.js');
+    $libraries = libraries_get_libraries();
+    dpm($libraries);
+  }
 }
