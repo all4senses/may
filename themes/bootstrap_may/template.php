8bb930a425b825afa3991c43e41803316edf2a20
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-23 14:32:10 +0600

    Scrolly testing

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -3,3 +3,11 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function bootstrap_may_preprocess_page(&$variables) {
+  dpm($variables);
+}
