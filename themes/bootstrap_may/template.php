6c178c4626fd7fcff9434aa105a5e6c508c87581
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-24 06:02:34 +0600

    Parallax testing

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -13,10 +13,11 @@ function bootstrap_may_preprocess_page(&$variables) {
   
   if ($variables['is_front']) {
     //drupal_add_js(libraries_get_path('select2').'/select2.js');
-    $libraries = libraries_get_libraries();
-    dpm($libraries);
+    //$libraries = libraries_get_libraries();
+    //dpm($libraries);
     
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
+    //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallaxx/jquery.data-parallaxx.js');
     
     $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
     drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
