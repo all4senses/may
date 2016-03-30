4d06d8bdb1418de88d4825d7d81718bb22ceb165
U-FLYSMALL\Alex <all4senses@gmail.com>
2016-03-31 05:18:48 +0600

    Parallax

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -16,10 +16,10 @@ function bootstrap_may_preprocess_page(&$variables) {
     //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
     
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx.js');
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/js/jquery.easing.1.3.plus_one_argument.js');
     
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll.js');
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
     
     
     $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
