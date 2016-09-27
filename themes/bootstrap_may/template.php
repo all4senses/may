b272085632e0aa6f219e975aa2bf23914402f6ca
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-09-27 16:27:15 +0700

    add new attributes to images

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -16,22 +16,23 @@ function bootstrap_may_preprocess_page(&$variables) {
     //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
     
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.easing.1.3.plus_one_argument.js');
     
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
     
     
     $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
     drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
-    
-    
     //drupal_add_library('contextual','contextual-links');
     
   }
 }
 
 
+
+
+/*
 function bootstrap_may_preprocess_node($variables) {
 //  if (drupal_get_path_alias("node/{$variables['#node']->nid}") == 'foo') {
 //    drupal_add_css(drupal_get_path('theme', 'MYTHEME') . "/css/foo.css");
@@ -43,3 +44,4 @@ function bootstrap_may_preprocess_node($variables) {
         drupal_add_js($node_parallax_js);
     }
 }
+*/
