3cca8600449ab0b0dfe633e090a688e21b8fabf2
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-09-27 16:40:28 +0700

    add new attributes to images

--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -16,9 +16,9 @@ function bootstrap_may_preprocess_page(&$variables) {
     //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
     
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.easing.1.3.plus_one_argument.js');
     
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
     
     
