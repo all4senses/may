17a51137f75866c769ecc81ac13fc647a1e1311d
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-09-27 16:16:59 +0700

    add new attributes to images

---
+++ modules/_May/may_pages/js/parallax/pn3.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+  Drupal.behaviors.may_home_parallax = {
+    attach: function (context, settings) {
+       
+      console.log('may_home_parallax.....');
+       
+//      $('.flexslider').flexslider({
+//        animation: "slide",
+//        directionNav: false
+//      });
+       
+    }
+  };
+
+}(jQuery));
--- themes/bootstrap_may/template.php
+++ themes/bootstrap_may/template.php
@@ -16,14 +16,16 @@ function bootstrap_may_preprocess_page(&$variables) {
     //drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/scrolly/jquery.scrolly.js');
     
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.requestAnimationFrame.min.js');
-    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.easing.1.3.plus_one_argument.js');
     
+    drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery-data-parallax---kasparsj---a4s/jquery.data-parallaxx---a4s.js');
     drupal_add_js('sites/all/libraries/jquery.plugins/_parallax/jquery.parallax-scroll---gsgd/js/jquery.parallax-scroll---a4s.js');
     
     
-    $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
-    drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
+//    $path_to_custom_js = drupal_get_path('module', 'may_pages') . '/js/';
+//    drupal_add_js($path_to_custom_js . 'may_home_parallax.js');
+    
+    
     //drupal_add_library('contextual','contextual-links');
     
   }
@@ -36,4 +38,8 @@ function bootstrap_may_preprocess_node($variables) {
 //  }
     
     dpm($variables, '$variables');
+    
+    if (file_exists(drupal_get_path('module', 'may_pages') . '/js/parallax/pn' . $variables['nid'])) {
+        drupal_add_js($node_parallax_js);
+    }
 }
