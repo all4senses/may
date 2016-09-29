ad10e022ee1ed9382b8a32b0f8c7de92c49a1f18
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-09-29 23:14:22 +0700

    CkEditor settings

--- modules/_Editor/ckeditor/plugins/imageresponsive/lang/en.js
+++ modules/_Editor/ckeditor/plugins/imageresponsive/lang/en.js
@@ -4,5 +4,6 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'imageresponsive', 'en', {
 	sizes: 'Sizes (responsive)',
-	srcset: 'Sourceset (responsive)'
+	srcset: 'Sourceset (responsive)',
+	data_a4s: 'data a4s',
 } );
