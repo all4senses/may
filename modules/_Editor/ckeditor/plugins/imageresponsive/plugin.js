3dca8bbaea7aa6ef6df42dbd463da68fa37d5ad4
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-09-30 00:22:07 +0700

    CkEditor settings

--- modules/_Editor/ckeditor/plugins/imageresponsive/plugin.js
+++ modules/_Editor/ckeditor/plugins/imageresponsive/plugin.js
@@ -21,13 +21,14 @@
                 if(!widget.allowedContent.img || !widget.allowedContent.img.attributes)
                     return;
 
+                    console.log(widget,'widget');
                 if(widget.allowedContent.img.attributes.indexOf('srcset') == -1)
                     widget.allowedContent.img.attributes += ',srcset'
                 if(widget.allowedContent.img.attributes.indexOf('sizes') == -1)
                     widget.allowedContent.img.attributes += ',sizes'
                 
-                if(widget.allowedContent.img.attributes.indexOf('data-a4s') == -1)
-                    widget.allowedContent.img.attributes += ',data-a4s'
+                if(widget.allowedContent.img.attributes.indexOf('data_a4s') == -1)
+                    widget.allowedContent.img.attributes += ',data_a4s'
             });
         },
         init: function(editor) {
@@ -36,8 +37,8 @@
                 var widget = e.data;
 
                 // figure out if this is the image dialog.
-//                if(widget.name != 'image')
-//                    return;
+                if(widget.name != 'image')
+                    return;
 
                 // register handler for data
                 widget.on('data', function(e) {
@@ -57,6 +58,8 @@
                         e.sender.parts.image.setAttribute('data-a4s', widget.data_a4s);
                     else
                         e.sender.parts.image.removeAttribute('data-a4s');
+                    
+                    console.log(e.sender.parts.image, 'e.sender.parts.image');
                 });
 
                 // set data from existing variables.
@@ -71,6 +74,7 @@
                     sizes: image.getAttribute( 'sizes' ) || '',
                     data_a4s: image.getAttribute( 'data-a4s' ) || ''
                 };
+                console.log(data,'data');
                 widget.setData(data);
             });
 
@@ -122,6 +126,8 @@
                         widget.setData('data-a4s', this.getValue());
                     }
                 }, 'alignment');
+                
+                console.log(infoTab, 'infoTab');
                 
             });
         }
