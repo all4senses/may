2fee85b66644ea45ce877d7fda0b329f18d90f14
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-10-01 04:44:43 +0700

    CkEditor settings

--- modules/_Editor/ckeditor/plugins/extra_img_attr_fields/plugin.js
+++ modules/_Editor/ckeditor/plugins/extra_img_attr_fields/plugin.js
@@ -39,14 +39,14 @@
                 
                 console.log(widget, 'widget +++');
                 // set data from existing variables.
-                var image = widget.element;
+                //var image = widget.element;
                 
                 // since the img-tag can be wrapped with a caption, make sure we use the right element.
 //                if(image.getName() != 'img')
 //                    image = image.findOne('img');
 
-                console.log(image, 'image +++');    
-                console.log(image.$.attributes, 'image $ attr +++'); // shows objects right away
+//                console.log(image, 'image +++');    
+//                console.log(image.$.attributes, 'image $ attr +++'); // shows objects right away
                 // 
                 // 
                 // 
