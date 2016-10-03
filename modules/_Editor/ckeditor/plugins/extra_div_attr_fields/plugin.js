34bacbe910ec829d94ed37020464f0b658f4cd26
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-10-04 00:41:37 +0700

    CKEditor Add new allowed attributes to tags

--- modules/_Editor/ckeditor/plugins/extra_div_attr_fields/plugin.js
+++ modules/_Editor/ckeditor/plugins/extra_div_attr_fields/plugin.js
@@ -106,6 +106,41 @@
                 checkField = info.get('elementStyle');
                 console.log(checkField, 'elementStyle before');
                 
+                
+                
+                // Reuse the 'stylescombo' plugin's styles definition.
+                // Registered 'CKEDITOR.style' instances.
+		var styles = {};
+                editor.getStylesSet( function( stylesDefinitions ) {
+                        var styleName, style;
+
+                        if ( stylesDefinitions ) {
+                                // Digg only those styles that apply to 'div'.
+                                for ( var i = 0; i < stylesDefinitions.length; i++ ) {
+                                        var styleDefinition = stylesDefinitions[ i ];
+                                        if ( styleDefinition.element && styleDefinition.element == 'div' ) {
+                                                styleName = styleDefinition.name;
+                                                styles[ styleName ] = style = new CKEDITOR.style( styleDefinition );
+
+//                                                if ( editor.filter.check( style ) ) {
+//                                                        // Populate the styles field options with style name.
+//                                                        stylesField.items.push( [ styleName, styleName ] );
+//                                                        stylesField.add( styleName, styleName );
+//                                                }
+                                        }
+                                }
+                        }
+
+//                        // We should disable the content element
+//                        // it if no options are available at all.
+//                        stylesField[ stylesField.items.length > 1 ? 'enable' : 'disable' ]();
+//
+//                        // Now setup the field value manually if dialog was opened on element. (#9689)
+//                        setTimeout( function() {
+//                                dialog._element && stylesField.setup( dialog._element );
+//                        }, 0 );
+                } );
+                                
                 checkField.setup = function( element ) {
 							for ( var name in styles )
                                                             styles[ name ].checkElementRemovable( element, true, editor ) && this.setValue( name, 1 );
