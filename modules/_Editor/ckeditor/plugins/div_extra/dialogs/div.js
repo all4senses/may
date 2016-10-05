c0be63ac81cc165ccef45d734a22a0abb432e3a2
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-10-06 02:59:05 +0700

    CKEditor div extra li

--- modules/_Editor/ckeditor/plugins/div_extra/dialogs/div.js
+++ modules/_Editor/ckeditor/plugins/div_extra/dialogs/div.js
@@ -274,7 +274,12 @@
                     
 		// @type divDialog
 		return {
-			title: 'Edit tag: ' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.nodeName, //editor.lang.div_extra.title,
+			title: 'Edit tag: ' 
+                                + '<' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.nodeName + '>'
+                                + (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id ? '' : ', id="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id.value + '"')
+                                + (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class ? '' : ', class="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class.value + '"')
+                        , 
+                        //editor.lang.div_extra.title,
                         /*
                             function() {
                                 //var el = Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index];
