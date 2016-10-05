adb27cad8342f9868fdc7e29b2d247be803efbac
Alexander Khrapovitskiy <all4senses@gmail.com>
2016-10-06 03:10:27 +0700

    CKEditor div extra li

--- modules/_Editor/ckeditor/plugins/div_extra/dialogs/div.js
+++ modules/_Editor/ckeditor/plugins/div_extra/dialogs/div.js
@@ -276,8 +276,8 @@
 		return {
 			title: 'Edit tag: ' 
                                 + '<' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.nodeName + '>'
-                                + (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id ? '' : ', id="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id.value + '"')
-                                + (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class ? '' : ', class="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class.value + '"')
+                                //+ (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id ? '' : ', id="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.id.value + '"')
+                                //+ (!Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class ? '' : ', class="' + Drupal.settings.cur_element_and_its_parents[current_element_or_its_parent_index].$.attributes.class.value + '"')
                         , 
                         //editor.lang.div_extra.title,
                         /*
